d5a3436df666a402dc3a4c574745e52f0741ce26
sunfkkc <rlcks77@nate.com>
2023-03-01 20:56:29 +0900

    refactor: enter의 로직을 TicketSeller에 위임

--- src/TicketSeller.java
+++ src/TicketSeller.java
@@ -5,7 +5,15 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
-    }
+    public void sellTo(Audience audience){
+        if( audience.getBag().hasInvitation()) {
+            audience.getBag().setTicket(ticketOffice.getTickets());
+        }
+        else {
+            Ticket ticket = ticketOffice.getTickets();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+        }
 }
