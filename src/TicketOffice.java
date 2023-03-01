bbb2bf42624b1f5b6b09c57e432c223cd1768b2e
sunfkkc <rlcks77@nate.com>
2023-03-01 21:57:53 +0900

    refactor: sellTo 메서드는 ticketOffice 인터페이스에 의존

--- src/TicketOffice.java
+++ src/TicketOffice.java
@@ -23,4 +23,8 @@ public class TicketOffice {
     public void plusAmount(Long amount) {
         this.amount += amount;
     }
+
+    public void sellTicketTo(Audience audience){
+        plusAmount(audience.buy(getTickets()));
+    }
 }
