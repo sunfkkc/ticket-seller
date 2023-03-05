41880ea933db93b5ecfb6ed87b2fb81241ff272b
sunfkkc <rlcks77@nate.com>
2023-03-05 21:52:53 +0900

    feat: 데이터 중심 영화 클래스 생성

--- src/data_driven_movie/Movie.java
+++ src/data_driven_movie/Movie.java
@@ -1,5 +1,19 @@
 package data_driven_movie;
 
+import movie.DiscountCondition;
+import movie.Money;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Movie {
     private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+
 }
