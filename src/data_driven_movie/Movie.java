c83dd5384019b4e3984de9a9c31a3ed86bc12085
sunfkkc <rlcks77@nate.com>
2023-03-05 22:15:38 +0900

    refactor: getter, setter 추가

--- src/data_driven_movie/Movie.java
+++ src/data_driven_movie/Movie.java
@@ -15,5 +15,59 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public void setRunningTime(Duration runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Duration getRunningTime() {
+        return runningTime;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
 }
