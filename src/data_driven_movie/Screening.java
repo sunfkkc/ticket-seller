32f2bc8646f83821c70262e8057c12725567db4a
sunfkkc <rlcks77@nate.com>
2023-03-06 15:25:53 +0900

    feat: Screening 클래스 생성

---
+++ src/data_driven_movie/Screening.java
@@ -0,0 +1,35 @@
+package data_driven_movie;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public void setWhenScreened(LocalDateTime whenScreened) {
+        this.whenScreened = whenScreened;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+}
