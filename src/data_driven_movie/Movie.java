c2af6a887562921a17d91abcce1111d28aa91d8e
sunfkkc <rlcks77@nate.com>
2023-03-06 17:01:36 +0900

    refactor: 영화 요금 계산을 Movie에 위임, 할인 가능 한지 체크하는 메서드 추가

--- src/data_driven_movie/Movie.java
+++ src/data_driven_movie/Movie.java
@@ -3,6 +3,7 @@ package data_driven_movie;
 import movie.Money;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Movie {
@@ -68,5 +69,45 @@ public class Movie {
 
     public double getDiscountPercent() {
         return discountPercent;
+    }
+
+
+    public Money calculateAmountDiscountedFee(){
+        if( movieType != MovieType.AMOUNT_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+
+        return  fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee(){
+        if( movieType != MovieType.PERCENT_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee(){
+        if( movieType != MovieType.NONE_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence){
+        for( DiscountCondition condition : discountConditions){
+            if( condition.getType() == DiscountConditionType.PERIOD){
+
+                if( condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) return true;
+
+
+            }else{
+                if( condition.isDiscountable(sequence)) return true;
+            }
+        }
+
+        return false;
     }
 }
