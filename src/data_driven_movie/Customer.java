d198ad75e6e88238ec7154c0d8554083f354de89
sunfkkc <rlcks77@nate.com>
2023-03-06 15:19:52 +0900

    feat: Customer 클래스 생성

---
+++ src/data_driven_movie/Customer.java
@@ -0,0 +1,12 @@
+package data_driven_movie;
+
+public class Customer {
+
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
