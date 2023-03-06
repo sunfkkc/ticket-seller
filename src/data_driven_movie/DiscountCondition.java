70b5aa2cdac7dccc13247a265850b07df068b604
sunfkkc <rlcks77@nate.com>
2023-03-06 16:41:17 +0900

    refactor: DiscountCondition 클래스에서 할인 여부를 판단

--- src/data_driven_movie/DiscountCondition.java
+++ src/data_driven_movie/DiscountCondition.java
@@ -53,4 +53,20 @@ public class DiscountCondition {
     public LocalTime getEndTime() {
         return endTime;
     }
+
+    public boolean isDiscountable( DayOfWeek dayOfWeek, LocalTime time){
+        if( type != DiscountConditionType.PERIOD){
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) && this.startTime.compareTo(time) <= 0 && this.endTime.compareTo(time) >= 0;
+    }
+
+    public boolean isDiscountable(int sequence){
+        if( type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
+    }
 }
