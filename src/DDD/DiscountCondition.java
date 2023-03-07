98a9a0859dee2f159c74f328c969e346bc091970
sunfkkc <rlcks77@nate.com>
2023-03-07 14:55:07 +0900

    refactor: (DDD) isSatisfiedBy 메서드 완성

--- src/DDD/DiscountCondition.java
+++ src/DDD/DiscountCondition.java
@@ -1,8 +1,39 @@
 package DDD;
 
+import data_driven_movie.DiscountConditionType;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 public class DiscountCondition {
 
+
+    private DiscountConditionType type;
+    private int sequence;
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
     public boolean isSatisfiedBy(Screening screening){
 
+        switch (type){
+            case PERIOD:
+                return isSatisfiedByPeriod(screening);
+            case SEQUENCE:
+                return isSatisfiedBySequence(screening);
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private boolean isSatisfiedByPeriod(Screening screening){
+
+        //대충 조건 충족 검사 로직
+        return true;
+    }
+
+    private boolean isSatisfiedBySequence(Screening screening){
+
+        //대충 조건 충족 검사 로직
+        return false;
     }
 }
