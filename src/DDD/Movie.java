85913f3e3b96f28d201081599b917388eb4ea621
sunfkkc <rlcks77@nate.com>
2023-03-07 12:26:46 +0900

    feat: (DDD) "계산하라" 의 책임을 수행할 객체 Movie 생성

---
+++ src/DDD/Movie.java
@@ -0,0 +1,32 @@
+package DDD;
+
+import data_driven_movie.MovieType;
+import movie.Money;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money calculateMovieFee(Screening screening){
+
+        if( isDiscountable( screening)){
+
+            return fee.minus( calculateDiscountAmount());
+        }
+
+        return fee;
+
+    }
+
+
+}
