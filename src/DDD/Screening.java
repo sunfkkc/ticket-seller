8db5c93a13b2c30a76805caadea98dd3deb2d3ea
sunfkkc <rlcks77@nate.com>
2023-03-07 12:08:27 +0900

    feat: DDD 시작, "예매하라" 의 책임을 수행할 객체 Screening 생성

---
+++ src/DDD/Screening.java
@@ -0,0 +1,21 @@
+package DDD;
+
+import movie.Money;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount){
+        return new Reservation( customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee( int audienceCount){
+
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+}
