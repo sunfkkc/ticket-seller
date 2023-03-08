1423a6fd6285d39617e40dde6c73f13b81f47fcb
sunfkkc <rlcks77@nate.com>
2023-03-08 16:22:24 +0900

    feat: (CQS) Event 클래스 생성

---
+++ src/command_query_sep/Event.java
@@ -0,0 +1,17 @@
+package command_query_sep;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+
+    private String subject;
+    private LocalDateTime from;
+    private Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+}
